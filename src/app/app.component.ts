9c3f0de23452eb50a5c9c9a4959c12c7063d776b
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-03-04 14:42:07 +0100

    easy website aufgesetzt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 // app.component.ts
 
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +13,17 @@ import { Component } from '@angular/core';
     <div>
       <h1>Meine Webseite</h1>
       <nav>
-        <a routerLink="/home">Home</a>
-        <a routerLink="/about">About</a>
-        <a routerLink="/contact">Contact</a>
+        <a [routerLink]="['home']">Home</a>
+        <a [routerLink]="['about']">About</a>
+        <a [routerLink]="['contact']">Contact</a>
       </nav>
     </div>
 
     <router-outlet></router-outlet>
-  `
+  `,
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, SharedModule, RouterModule],
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'meine-webseite';
